63971d4bdb0901f0c427ffcc75c3ae24e7100e15
ArjunKushwaha01 <kushwahaarjun920@gmail.com>
2025-03-28 00:13:50 +0530

    symmetric encryption and decryption with java

--- src/classes/encryptdecryptclass.java
+++ src/classes/encryptdecryptclass.java
@@ -2,7 +2,9 @@ package classes;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class encryptdecryptclass {
 
@@ -27,10 +29,42 @@ public class encryptdecryptclass {
     private int len;
     private int maxlen;
 
+    // Create constructor
 
     public encryptdecryptclass(String input, String al) throws  Exception {
+        // Generate Key
         keygen = KeyGenerator.getInstance(al);
         secretkey= keygen.generateKey();
+
+        // Parameter Setting
+        allcipher = Cipher.getInstance(al+"/CBC/PKCS5Padding");
+        allcipher.init(Cipher.ENCRYPT_MODE, secretkey);
+
+        //Create Plaintext
+        plaintext = input.getBytes(StandardCharsets.UTF_8);
+    }
+    //Getter method to return encryption result
+    public String encrypt() throws Exception {
+        // start encryption
+        ciphertext = allcipher.doFinal(plaintext);
+
+        //Covert the encryption output to string
+
+        strciphertext = Base64.getEncoder().encodeToString(ciphertext);
+        return strciphertext;
+    }
+
+    //Getter method to return the decryption result
+
+    public String decrypt() throws Exception {
+        //parameter setting
+
+        allcipher.init(Cipher.DECRYPT_MODE, secretkey, allcipher.getParameters());
+        //Start Decrypt
+        decrypttext = allcipher.doFinal(ciphertext);
+        //Convert the decryption result to string
+        strciphertext = new String((decrypttext, StandardCharsets.UTF_8);
+        return strciphertext;
     }
 }
 
